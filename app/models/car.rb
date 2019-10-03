d768b5f78bf580b5e6aaaa71a0261435f3f3b9b0
WarrenJRamos <wramos1@msudenver.edu>
2019-10-03 15:11:50 -0600

    validation for car

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,10 +3,9 @@ class Car < ApplicationRecord
   has_many :car_parts
   has_many :parts, through: :car_parts
 
+  validates :make, presence: true
   validates_associated :parts
-
-  validates :vin_number, uniqueness: true
-  validates :vin_number, format: { with: /[0-9]/}
-  validates :vin_number, length: { in: 0..4 }
-  validates :vin_number, numericality: { only_integer: true }
+  validates :vin_number, presence: true, uniqueness: true, 
+    format: { with: /[0-9]/}, length: { is: 4 }, numericality: { only_integer: true }
+  validates :model_title, presence: true, length: { in: 2..20 }
 end
