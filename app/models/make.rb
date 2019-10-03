98c154ebaaea132d77775dca151c506ce0e9c72f
WarrenJRamos <wramos1@msudenver.edu>
2019-10-03 15:11:04 -0600

    validate for make

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,6 +1,7 @@
 class Make < ApplicationRecord
     has_many :cars
 
+    validates_associated :cars
     validates :name, length: { minimum: 2 }
     validates :country, length: { minimum: 2 }
 end
