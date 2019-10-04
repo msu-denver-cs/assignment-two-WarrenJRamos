cb706b8b3384162cd9557cf8475cc058eb3ec257
WarrenJRamos <wramos1@msudenver.edu>
2019-10-04 12:05:28 -0600

    Added some tests for the car_controller.

--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -5,6 +5,19 @@ class CarsControllerTest < ActionDispatch::IntegrationTest
     @car = cars(:one)
   end
 
+  test "shouldn't find a car" do
+    assert Car.where("model_title like ?", "NOT A MODEL").length == 0
+  end
+
+  test "should find cars from the fixture" do
+    assert Car.where("model_title like ?", "4Runner").length == 1
+  end
+
+  test "searches always return 200" do
+    get search_cars_url, params: { search: "Bob" }
+    assert_equal 200, status
+  end
+
   test "should get index" do
     get cars_url
     assert_response :success
@@ -17,11 +30,26 @@ class CarsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create car" do
     assert_difference('Car.count') do
-      post cars_url, params: { car: { make_id: @car.make_id, model_title: @car.model_title, vin_number: @car.vin_number } }
+      post cars_url, params: { car: { make_id: @car.make_id, model_title: @car.model_title, vin_number: 1999 } }
     end
 
     assert_redirected_to car_url(Car.last)
   end
+
+  test "should find 4Runner" do
+    get search_cars_url, params: { search: "4Runner" }
+    assert_select 'td', '4Runner'
+  end 
+
+  test "should find 1000" do
+    get search_cars_url, params: { search: "1000" }
+    assert_select 'td', '1000'
+  end 
+
+  #test "shouldn't find MyString" do
+  #  get search_cars_url, params: { search: "MyString" }
+  #  assert_select 'td', 16
+  #end
 
   test "should show car" do
     get car_url(@car)
@@ -34,7 +62,7 @@ class CarsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update car" do
-    patch car_url(@car), params: { car: { make_id: @car.make_id, make_title: @car.make_title, model_title: @car.model_title, vin_number: @car.vin_number } }
+    patch car_url(@car), params: { car: { make_id: @car.make_id, model_title: @car.model_title, vin_number: @car.vin_number } }
     assert_redirected_to car_url(@car)
   end
 
