ee2eb099fac20ce412178ae767d7f677c7f79426
WarrenJRamos <wramos1@msudenver.edu>
2019-10-15 14:05:03 -0600

    rails generate devise User and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_043205) do
+ActiveRecord::Schema.define(version: 2019_10_15_200251) do
 
   create_table "car_parts", force: :cascade do |t|
     t.integer "car_id"
@@ -41,6 +41,18 @@ ActiveRecord::Schema.define(version: 2019_10_02_043205) do
     t.string "part_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
